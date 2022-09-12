fa01e03b4ee66bd22615596ee9d62e029393ef28
bem130 <haruki070617@gmail.com>
2022-09-12 23:54:34 +0900

    change a little

--- reversi.js
+++ reversi.js
@@ -16,15 +16,21 @@ class Reversi {
     choose(x,y) {
         console.log(`player${this.nextplayer} chose the ${[x,y]}`);
         let torevers = this.search(x,y,this.nextplayer);
-        this.set(x,y,this.nextplayer);
-        for (let cell of torevers) {
-            console.log(cell)
-            this.set(cell[0],cell[1],this.nextplayer);
+        if (torevers.length>0) {
+            this.set(x,y,this.nextplayer);
+            for (let cell of torevers) {
+                console.log(cell)
+                this.set(cell[0],cell[1],this.nextplayer);
+            }
+            this.nextplayer = this.gnextplayer();
         }
-        this.nextplayer = this.gnextplayer();
+        else {
+            console.error("can't choose the cell");
+        }
     }
     search(x,y,player) {
         let reversal = [];
+        if (this.board[y*8+x]==1||this.board[y*8+x]==2) {return reversal;}
         let leftreversal = [];let toleftr = true; // 左方向
         for (let sx=x-1;sx>=0;sx--) {
             if (this.get(sx,y)==0) {toleftr = false;break;}
@@ -33,7 +39,7 @@ class Reversi {
         }
         if (toleftr) {reversal=reversal.concat(leftreversal);}
         let rightreversal = [];let torightr = true; // 右方向
-        for (let sx=x;sx<8;sx++) {
+        for (let sx=x+1;sx<8;sx++) {
             if (this.get(sx,y)==0) {torightr = false;break;}
             else if (this.get(sx,y)==player) {break;}
             else {rightreversal.push([sx,y]);}
@@ -47,23 +53,16 @@ class Reversi {
         }
         if (totopr) {reversal=reversal.concat(topreversal);}
         let bottomreversal = [];let tobottomr = true; // 下方向
-        for (let sy=y;sy<8;sy++) {
+        for (let sy=y+1;sy<8;sy++) {
             if (this.get(x,sy)==0) {tobottomr = false;break;}
             else if (this.get(x,sy)==player) {break;}
             else {bottomreversal.push([x,sy]);}
         }
         if (tobottomr) {reversal=reversal.concat(bottomreversal);}
-
-        console.log(reversal);
         return reversal;
     }
     gnextplayer() {
-        if (this.nextplayer==1) {
-            return 2;
-        }
-        else {
-            return 1;
-        }
+        if (this.nextplayer==1) {return 2;} return 1;
     }
     getarr() {
         let arr = [];
